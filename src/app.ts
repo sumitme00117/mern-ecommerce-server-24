63fee7f338bd828c909e38ef760b4159b3a90277
Sumit Srivastava <sumitme00117@gmail.com>
2024-09-14 00:04:54 +0530

    Including Client URL

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,11 @@ export const myCache = new NodeCache()
 const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(cors())
+app.use(cors({
+    origin: [process.env.CLIENT_URL!],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }))
 app.get("/", (req,res) => {
     res.send("API working with /api/v1")
 })
